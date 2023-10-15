1614a08c370821256b4587b012f9a990cc823391
Serhii Paziuk <pazyk.15@gmail.com>
2023-10-15 20:07:34 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function ask(question) {
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      resolve(answer);
+    });
+  });
+}
+
+function close() {
+  rl.close();
+}
+
+module.exports = { close, ask };
